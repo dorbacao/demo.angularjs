cdfd5f779f66faf331a272d14c40626349ebf8da
Marcus Dorbacao <marcus.dorbacao@bsitecnologia.com.br>
2013-06-26 17:29:18 -0300

    criacao exemplo 1

---
+++ exemplo1/todo.js
@@ -0,0 +1,16 @@
+function ListaComprasController($scope){
+
+	
+
+$scope.itens = [
+	{produto: 'Leite', quantidade:2, comprado:false},
+	{produto: 'Coca-Cola', quantidade:12, comprado:true}
+];
+
+$scope.adicionaItem = function(newItem){	
+	var prd = {produto:newItem.produto, quantidade:newItem.produto, comprado:false};
+	itens.push(prd);
+}
+
+}
+
