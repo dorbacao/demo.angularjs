edf6d5f73fd53ee6b948a6dd37661c12495cab94
Marcus Dorbacao <marcus.dorbacao@bsitecnologia.com.br>
2013-06-27 18:46:29 -0300

    exemplo1

--- exemplo1/todo.js
+++ exemplo1/todo.js
@@ -1,14 +1,54 @@
-function ListaComprasController($scope){
+var todo = angular.module('todo',[]).config(function($routeProvider){
 
-$scope.itens = [
-	{produto: 'Leite', quantidade:2, comprado:false},
-	{produto: 'Coca-Cola', quantidade:12, comprado:true}
-];
+	$routeProvider.when('/login',{
+			templateUrl:'Login.html',
+			controller:'LoginController'
+	});
 
-$scope.adicionaItem = function(newItem){	
-	var prd = {produto:newItem.produto, quantidade:newItem.produto, comprado:false};
-	itens.push(prd);
-}
+	$routeProvider.when('/home',{
+			templateUrl:'Home.html',
+			controller:'HomeController'
+	});
 
-}
+	$routeProvider.otherwise({redirectTo:'/login'});
+
+});
+
+todo.controller('LoginController', function($scope, $location){
+	
+	$scope.credencial = {login:'', senha:''};
+
+	$scope.logar = function ()
+	{
+		var login = $scope.credencial.login;
+		var senha = $scope.credencial.senha;
+
+		if(login == 'dorba' && senha == '845756'){
+			$location.path('/home');
+		}
+
+		else{alert('Usuário e/ou Senha Inválido')}
+	};
+});
+
+todo.controller('HomeController', function($scope, $location){
+
+	$scope.tarefa = {nome:'', data:''};
+	$scope.tarefas = [];
+
+	$scope.adicionar = function (){
+		var valNome = $scope.tarefa.nome;
+		var valData = $scope.tarefa.data;
+
+		$scope.tarefas.push({nome:valNome, data:valData});
+	};
+
+	$scope.excluir = function ($index){
+		
+		$scope.tarefas.splice($index, 1 );
+	};
+});
+
+
+
 
